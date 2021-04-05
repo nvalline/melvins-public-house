4fb42af7b969d294249868373067c8dc4d78f339
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-04-05 11:30:04 -0600

    file cleanup

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-const mobileBars = document.getElementById("mobileBars");
-
-// Toggle Mobile Nav
-function showNav() {
-    console.log("HIT")
-    document.getElementById("mobileNav").classList.toggle("hide")
-}
-
-mobileBars.addEventListener("click", showNav);
