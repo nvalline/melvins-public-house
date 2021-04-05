75e697aac831dc4d334f913545db5b7e526f8116
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-04-05 12:29:29 -0600

    connected server & db

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 // dependencies
 const express = require('express');
-const session = require('express-session');
+const mongoose = require('mongoose');
+
+// require environmental variables
+require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+// MongoDB connection
+mongoose
+	.connect(process.env.MONGODB_URI || process.env.MONGO_LOCAL_URI, {
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(console.log('MongoDB connected'))
+	.catch((err) => console.log('MDB Connection Error:', err));
+
+// Connect Express Server
 app.listen(PORT, () => {
 	console.log(`Server listening on PORT: ${PORT}`);
 });
