bd1738f917119427570366c0c52bc6536bb64be7
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-04-06 22:10:14 -0600

    added hero img & adjusted nav styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // components
 import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
 
 function App() {
 	return (
 		<Router>
 			<main>
 				<Navbar />
+				<Hero />
 				<p className='main-app'>App</p>
 			</main>
 		</Router>
---
+++ client/src/components/hero/Hero.js
@@ -0,0 +1,14 @@
+// dependencies
+import React from 'react';
+// images
+// import heroImage from '../../img/beertaps2.jpg';
+
+const hero = () => {
+	return (
+		<section className='hero-container'>
+			{/* <div className='up-tab-black'></div> */}
+		</section>
+	);
+};
+
+export default hero;
