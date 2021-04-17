a716dbb8e8a217d20452349213f0620d5e22c907
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-04-16 18:52:00 -0600

    updated about content & basic media styles

--- client/src/components/about/AboutContent.js
+++ client/src/components/about/AboutContent.js
@@ -10,23 +10,30 @@ const AboutContent = () => {
 			</div>
 			<article className='bio-block'>
 				<p className='bio ft-white'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-					similique reiciendis! Exercitationem inventore sunt minima voluptates
-					qui officiis doloribus odit voluptas, voluptatibus animi distinctio
-					numquam nostrum rerum id molestiae eius hic!
+					Melvins Public House is the product of a house divided. Melissa
+					(a.k.a. Mel or Melvin) a Cleveland Browns & Ohio State fan and Mike a
+					Pittsburg Steelers & Penn State fan, met in the neighboring Wasatch
+					mountain town Park City and now call Heber their home. Enthusiasts of
+					the local area, they found that it was an easy decision to open a true
+					locals hangout where every fan has a place to support, cheer and
+					commiserate with other locals over good food and drinks.
 				</p>
 				<p className='bio ft-white'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-					similique reiciendis! Exercitationem inventore sunt minima voluptates
-					qui officiis doloribus odit voluptas, voluptatibus animi distinctio
-					numquam nostrum rerum id molestiae eius hic!
+					They earned there place in the local food and beverage trade well
+					before opening this local’s favorite sports bar in Heber City and now
+					this historic site offers a family friendly atmosphere filled with big
+					screens, tasty food and a full bar serving cold drinks. Staying true
+					to a locals spot, Melvins is a fun place for more than just sports,
+					you can find live music and other events and specials that fill the
+					weekly calendar for all to enjoy.
 				</p>
-				{/* <p className='bio ft-white'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-					similique reiciendis! Exercitationem inventore sunt minima voluptates
-					qui officiis doloribus odit voluptas, voluptatibus animi distinctio
-					numquam nostrum rerum id molestiae eius hic!
-				</p> */}
+				<p className='bio ft-white'>
+					When the snow melts and the mountains and valleys turn green with
+					vegetation, be sure to step out and enjoy the outdoor patio. A perfect
+					spot to rest and refuel the body as you are passing through or even
+					finish a long day at work. It doesn’t take long to be welcomed as part
+					of the Melvins family and familiar faces become new friends.
+				</p>
 			</article>
 		</>
 	);
