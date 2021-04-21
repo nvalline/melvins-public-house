ab022171cf721a2b984cd6a4a7c6c15145cf245a
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-04-20 22:04:17 -0600

    added social links to desktop footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,13 +1,27 @@
 // dependencies
 import React from 'react';
+import { Link } from 'react-router-dom';
+// components
+import { SocialLinks } from '../../Links';
 
 const Footer = () => {
 	return (
 		<footer className='footer-container'>
 			<div className='yellow-up-tab'></div>
 			<div className='trim'></div>
-			{/* enter dynamic copyright year */}
-			<p className='footer-content'>copyright 2021 | Melvins Public House</p>
+			<div className='footer-content-block'>
+				{/* enter dynamic copyright year */}
+				<p className='footer-content'>copyright 2021 | Melvins Public House</p>
+				<div className='social-links'>
+					{SocialLinks.map((link, index) => {
+						return (
+							<Link to={link.url} key={index}>
+								<i className={link.className}></i>
+							</Link>
+						);
+					})}
+				</div>
+			</div>
 		</footer>
 	);
 };
